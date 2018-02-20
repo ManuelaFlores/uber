dcec1b3752367cd7ba6fc467a0297cff4b6c824c
Rocciescobar <roccisen.88@gmail.com>
2018-02-20 17:26:50 -0500

    Maquetado inicial

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    var app = firebase.app();
+    var features = ['auth', 'database', 'messaging', 'storage'].filter(function (feature) {
+      return typeof app[feature] === 'function';
+    });
+    document.getElementById('load').innerHTML = 'Firebase SDK loaded with ' + features.join(', ');
+  } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
+});
