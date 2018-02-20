8f584ae013ff6fc331282725e914f16bb239ba48
Rocciescobar <roccisen.88@gmail.com>
2018-02-20 18:54:04 -0500

    Solucionando conflicto

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,10 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-  // // The Firebase SDK is initialized and available here!
-  //
-  // firebase.auth().onAuthStateChanged(user => { });
-  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-  // firebase.messaging().requestPermission().then(() => { });
-  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-  //
-  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-
-  try {
-    var app = firebase.app();
-    var features = ['auth', 'database', 'messaging', 'storage'].filter(function (feature) {
-      return typeof app[feature] === 'function';
-    });
-    document.getElementById('load').innerHTML = 'Firebase SDK loaded with ' + features.join(', ');
-  } catch (e) {
-    console.error(e);
-    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-  }
+// Añade marcador de autos
+var auto1 = 'assets/icons/auto1.png';
+var autoMarker = new google.maps.Marker({
+  position: { lat: myLatit,
+    lng: myLongit },
+  map: map,
+  icon: image
 });
